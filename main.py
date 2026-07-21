3b1a7f6c297fe0459a556f98081c5ef49147090f
andreaskrebbers <andreaskrebbers@gmail.com>
2026-07-21 08:24:08 +0800

    Integrate weather API and Twilio for notifications
    Removed email functionality and integrated weather API with Twilio for rain notifications.

--- main.py
+++ main.py
@@ -1,42 +1,37 @@
-import csv
-import datetime as dt
-import csv
-import random
+import json
+import requests
+from twilio.rest import Client
 import os
-import smtplib
-birthday_list={}
-now=dt.datetime.now()
-month=now.month
-day=now.day
-annual_day=(f"{month},{day}")
+
+URL= "http://api.openweathermap.org/data/2.5/forecast"
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+apikey = os.environ.get("OPENWEATHER_API_KEY")
+
+LAT=22.365516
+LNG=114.251748
 
 
-def send_email():
-    folder_path = "./letter_templates"
-    text_files = [f for f in os.listdir(folder_path) if f.endswith('.txt')]
-    random_file = random.choice(text_files)
-    full_file_path = os.path.join(folder_path, random_file)
-    with open(full_file_path, mode="r", encoding="utf-8") as file:
-        file_contents = file.read()
-        updated_contents = file_contents.replace("[NAME]", row['name'])
-        msg_body = updated_contents
-        msg_body = (f"Subject: HAPPY BIRTHDAY\n\n{updated_contents}")
-        print(msg_body)
-    my_email = "p61668338@gmail.com"
-    password = "cskr nnag azkb kgoh"
-    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-         connection.starttls()
-         connection.login(user=my_email, password=password)
-         connection.sendmail(from_addr=my_email,
-                              to_addrs="p61668338@gmail.com",
-                              msg= msg_body)
+weather_parameters={"lat":LAT,"lon":LNG,"appid":OPENWEATHER_API_KEY,"cnt":4}
 
-with open ("birthdays.csv",mode="r") as file:
-    reader = csv.DictReader(file)
-    birthday_list=list(reader)
-    for row in birthday_list:
-        if int(row['month']) == month  and int(row['day']) == day:
-           send_email()
+response= requests.get(URL, params=weather_parameters)
+response.raise_for_status()
+results= response.json()
+
+
+will_rain = False
+for i in range(4):
+    weather_id = results["list"][i]["weather"][0]["id"]
+    if weather_id < 700:
+        will_rain = True
+if will_rain:
+    twilio_client = Client(account_sid, auth_token)
+    message = twilio_client.messages.create(
+        body="It is going to rain today. Bring an umbrella! 🌧️",
+        from_ = TWILIO_VIRTUAL_NBR,
+        to=TWILIO_VERIFIED_NBR
+    )
+    print(message.status)
 
 
 
